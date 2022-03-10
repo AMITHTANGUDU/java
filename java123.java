6975e71e7906037c694ab12318036014d1db49a8
TANGUDU AMITH KMAR <98936077+AMITHTANGUDU@users.noreply.github.com>
2022-03-10 11:58:57 +0530

    Update java123.java

--- java123.java
+++ java123.java
@@ -2,6 +2,6 @@ package java123;
 
 public class java123 {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, amith"); 
     }
 }
