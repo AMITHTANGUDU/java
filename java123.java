432e195bdf8abde5f9b4cb2261c88095c8039791
vardhan0809 <tanguduamithkumar.19.it@anits.edu.in>
2022-06-24 11:22:34 +0530

    second

--- java123.java
+++ java123.java
@@ -3,5 +3,6 @@ package java123;
 public class java123 {
 	public static void main(String[] args) {
         System.out.println("Hello, amith"); 
+        System.out.println("Hello, amith kumar");
     }
 }
